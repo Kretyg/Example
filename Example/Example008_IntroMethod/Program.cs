dd605eb251048c7c9673e98421d3e5ec9c09fc70
Kretyg <sokolovsky.zhora58@gmail.com>
2023-01-17 08:17:22 +0300

    вроде исправил

--- Example/Example008_IntroMethod/Program.cs
+++ Example/Example008_IntroMethod/Program.cs
@@ -1,16 +1,16 @@
 ﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
-    if(arg1> result) result = arg2;
+    if(arg2> result) result = arg2;
     if(arg3> result) result = arg3;
     return result;
 }
-int a1 = 15000000;
+int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
 int b2 = 233333;
-int c2 = 3300000;
+int c2 = 33;
 int a3 = 13333;
 int b3 = 23;
 int c3 = 313;
